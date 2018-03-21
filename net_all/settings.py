ba8856cbc4241174e914d07bfdef03474cd1acef
toha-livs <tohalivs>
2018-03-21 20:35:46 +0200

    start

--- net_all/settings.py
+++ net_all/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'k(j6t!!vivyq2r(uu2-v!e$_kp=$pecsus479q&wec7^nz@*a^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -92,6 +92,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
@@ -139,3 +143,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
